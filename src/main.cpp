3753ac02a0bef3e4e8cb07fac3e0c0f88d4245a1
Nate Epps <nathanael.epps@gmail.com>
2022-09-21 19:33:00 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// main.cpp
+// Nathanael Epps
+//
+
+#include <iostream>
+
+int main(int argc, const char** argv) {
+    std::cout << argv[0] << " v" << NCC_VERSION << "\n";
+}
+
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+//
+// test.cpp
+// Nathanael Epps
+//
+
+#include <iostream>
+
+int main(int argc, const char** argv) {
+    std::cout << argv[0] << " v" << NCC_VERSION << "\n";
+}
