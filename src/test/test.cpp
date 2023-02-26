f1d175fc4e75a71e8343298df9427dfa40b94129
Nate Epps <nathanael.epps@gmail.com>
2023-02-26 01:20:26 -0500

    Time tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -4,9 +4,17 @@
 //
 
 #include "System.hpp"
-#include "Util.hpp"
+#include <chrono>
+#include <iostream>
 
 int main(int argc, const char** argv) {
-    ncc::test::System s;
-    return s.run(ncc::util::bundle(argc, argv));
+    ncc::test::System system;
+
+    auto start = std::chrono::system_clock::now();
+    int rval = system.run(ncc::util::bundle(argc, argv));
+    auto end = std::chrono::system_clock::now();
+
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "\nTest time " << ms << "ms\n";
+    return rval;
 }
